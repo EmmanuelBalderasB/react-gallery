1c2a8232e5198c9943b69b340865a02ead6e2e75
Emmanuel <ebalderasbeq@gmail.com>
2024-04-23 01:33:56 -0600

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,38 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @next/next/no-img-element */
 //import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "../server/db/index";
 
 export const dynamic = "force-dynamic";
 
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} className="" alt="placeholder" />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, {desc}) => desc(model.id),
-  });
+
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex flex-col">
-            <img src={image.url} className="" alt="placeholder" />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
